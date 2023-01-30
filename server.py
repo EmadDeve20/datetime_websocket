530749ad353d5302fee5d5afac22dc073ed0d48c
EmadDeve20 <emaddeve20@gmail.com>
2023-01-30 14:14:41 +0330

    add server sid

---
+++ server.py
@@ -0,0 +1,21 @@
+import asyncio
+import websockets
+import datetime
+
+async def send_localtime(websocket):
+# async def send_localtime():
+        
+    date_time = datetime.datetime.now()
+    date_time = "[%d %02d %02d]\t%02d:%02d:%02d" % (date_time.year, date_time.day, date_time.month, date_time.hour, date_time.minute, date_time.second)
+    # async for message in datetime:
+    await websocket.send(date_time)
+    
+async def main():
+    print("Server Is UP!")
+    async with websockets.serve(send_localtime, "localhost", 9090):
+        await asyncio.Future()
+
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    pass
